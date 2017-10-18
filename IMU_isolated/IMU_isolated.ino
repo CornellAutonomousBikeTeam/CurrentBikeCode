0e12bfeb91160731c42c7435ea42be43a2342f99
Bobby Villaluz <30696102+BobbyVillaluz@users.noreply.github.com>
2017-10-18 17:31:29 -0400

    Update IMU_isolated.ino

--- IMU_isolated/IMU_isolated.ino
+++ IMU_isolated/IMU_isolated.ino
@@ -2,7 +2,7 @@
 
 void setup() {
   initIMU();
-  //Serial.begin(19200); //Testing purposes - Serial Monitor
+  Serial.begin(19200); //Testing purposes - Serial Monitor
 }
 
 struct roll_t {
@@ -11,32 +11,37 @@ struct roll_t {
   float yaw;
 };
 
+//struct that contains roll angle, roll rate, yaw
 roll_t imu_data;
+//array that contains euler angles in pitch, yaw, roll order
 float euler_angles[3];
+//array that contains the corrected gyro rate in radians/sec
 float gyro_rate[3];
 
+//Main loop
 void loop() {
+  //Send the "Get tared orientation as euler angles" command
   Serial1.write(":1\n");
   Serial.print("Tared orientation: ");
   readBuffer("euler");
-  roll_data.roll_angle = euler_angles[2];
-  roll_data.yaw = euler_angles[1];
+  imu_data.roll_angle = euler_angles[2];
+  imu_data.yaw = euler_angles[1];
   Serial1.write(":38\n");
   Serial.print("Gyro rate: ");
   readBuffer("gyro");
-  roll_data.roll_rate = gyro_rate[2];
+  imu_data.roll_rate = gyro_rate[2];
   Serial.println("Roll angle: ");
-  Serial.println(roll_data.roll_angle, 10);
+  Serial.println(imu_data.roll_angle, 10);
   //Serial.print(euler_angles[2], 10);
   Serial.println("Roll rate: ");
-  Serial.println(roll_data.roll_rate, 10);
+  Serial.println(imu_data.roll_rate, 10);
   //Serial.print(gyro_rate[2], 10);
   Serial.println("Yaw: ");
-  Serial.println(roll_data.yaw, 10);
+  Serial.println(imu_data.yaw, 10);
   //Serial.print(euler_angles[1], 10);*/
 }
 
-void readBuffer(String command) {
+roll_t readBuffer(String command) {
   int i = 0;
   String data;
   while(Serial1.available()){
@@ -62,5 +67,8 @@ void readBuffer(String command) {
       }
       Serial.print(ch);
     }
+  }
+  if (command.equals("euler")) {
+    return
   }
 }
