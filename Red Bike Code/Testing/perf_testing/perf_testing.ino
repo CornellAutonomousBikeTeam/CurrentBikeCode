4279299f3602f7e3ad1b0a856879d63b281f57e9
thezenelson <thezenelson@gmail.com>
2022-11-02 15:08:44 -0400

    Perf testing and finalized fast IO

--- Red Bike Code/Testing/perf_testing/perf_testing.ino
+++ Red Bike Code/Testing/perf_testing/perf_testing.ino
@@ -1,61 +1,22 @@
-#define A 0
-#define B 1
-#define mkpin(port, pin) ((port<<5) + pin)
-#define D0  mkpin(A, 22)
-#define D1  mkpin(A, 23)
-#define D2  mkpin(A, 10)
-#define D3  mkpin(A, 11)
-#define D4  mkpin(B, 10)
-#define D5  mkpin(B, 11)
-#define D6  mkpin(A, 20)
-#define D7  mkpin(A, 21)
-#define D8  mkpin(A, 16)
-#define D9  mkpin(A, 17)
-#define D10 mkpin(A, 19)
-#define D11 mkpin(A, 08)
-#define D12 mkpin(A, 09)
-#define D13 mkpin(B, 23)
-#define D14 mkpin(B, 22)
-#define D15 mkpin(A, 02)
-#define D16 mkpin(B, 02)
-#define D17 mkpin(B, 03)
-#define D18 mkpin(A, 04)
-#define D19 mkpin(A, 05)
-#define D20 mkpin(A, 06)
-#define D21 mkpin(A, 07)
-
-#define OutSet(input) (PORT_IOBUS->Group[input>>5].OUTSET.reg = 1<<(input & 0x1f))  // Make high
-#define OutClr(input) (PORT_IOBUS->Group[input>>5].OUTCLR.reg = 1<<(input & 0x1f))  // Make low
-#define OutTgl(input) (PORT_IOBUS->Group[input>>5].OUTTGL.reg = 1<<(input & 0x1f))  // toggle value
-
-#define Input(input)  (PORT_IOBUS->Group[input>>5].DIRSET.reg = 1<<(input & 0x1f))  // Make input
-#define Output(input) (PORT_IOBUS->Group[input>>5].DIRCLR.reg = 1<<(input & 0x1f))  // Make output
-// #define DIRTgl(input) (PORT_IOBUS->Group[input>>5].DIRTGL.reg = 1<<(input & 0x1f))  // toggle in/out
-
-#define Read(input)  ((PORT_IOBUS->Group[input>>5].IN.reg) >> (input & 0x1f) & 1)   // Read pin
-
 
 void setup() {
   Serial.begin(1000000);
-
-  Input(D0);
 }
 
 void loop() {
-  int v;
   unsigned long start;
 
   start = micros();
   for (unsigned int i = 0; i < 1000000; i++){
-    v = Read(D0);
+    // v1 test code
   }
-  Serial.print("fast: ");
+  Serial.print("v1: ");
   Serial.print  (1.0 / ((micros() - start) / 1000000.0));
 
   start = micros();
   for (unsigned int i = 0; i < 1000000; i++){
-    v = digitalRead(0);
+    // v2 test code
   }
-  Serial.print(", slow: ");
+  Serial.print(", v2: ");
   Serial.println(1.0 / ((micros() - start) / 1000000.0));
 }
---
+++ Red Bike Code/fast_IO/fast_IO.ino
@@ -0,0 +1,43 @@
+#define A 0
+#define B 1
+#define mkpin(port, pin) ((port<<5) + pin)
+
+#define D0  mkpin(A, 22)
+#define D1  mkpin(A, 23)
+#define D2  mkpin(A, 10)
+#define D3  mkpin(A, 11)
+#define D4  mkpin(B, 10)
+#define D5  mkpin(B, 11)
+#define D6  mkpin(A, 20)
+#define D7  mkpin(A, 21)
+#define D8  mkpin(A, 16)
+#define D9  mkpin(A, 17)
+#define D10 mkpin(A, 19)
+#define D11 mkpin(A, 08)
+#define D12 mkpin(A, 09)
+#define D13 mkpin(B, 23)
+#define D14 mkpin(B, 22)
+#define D15 mkpin(A, 02)
+#define A0 D15
+#define D16 mkpin(B, 02)
+#define A1 D16
+#define D17 mkpin(B, 03)
+#define A2 D17
+#define D18 mkpin(A, 04)
+#define A3 D18
+#define D19 mkpin(A, 05)
+#define A4 D19
+#define D20 mkpin(A, 06)
+#define A5 D20
+#define D21 mkpin(A, 07)
+#define A6 D21
+
+#define OutSet(pin) (PORT_IOBUS->Group[pin>>5].OUTSET.reg = 1<<(pin & 0x1f))  // Make high
+#define OutClr(pin) (PORT_IOBUS->Group[pin>>5].OUTCLR.reg = 1<<(pin & 0x1f))  // Make low
+#define OutTgl(pin) (PORT_IOBUS->Group[pin>>5].OUTTGL.reg = 1<<(pin & 0x1f))  // toggle value
+
+#define Input(pin)  (PORT_IOBUS->Group[pin>>5].DIRSET.reg = 1<<(pin & 0x1f))  // Make input
+#define Output(pin) (PORT_IOBUS->Group[pin>>5].DIRCLR.reg = 1<<(pin & 0x1f))  // Make output
+// #define DIRTgl(pin) (PORT_IOBUS->Group[pin>>5].DIRTGL.reg = 1<<(pin & 0x1f))  // toggle in/out
+
+#define Read(pin)  ((PORT_IOBUS->Group[pin>>5].IN.reg) >> (pin & 0x1f) & 1)   // Read pin
