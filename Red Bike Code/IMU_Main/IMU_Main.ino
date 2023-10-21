8d4eecba35c0391b6837c05517a6a3bbddba2906
Zachary Evan Nelson <thezenelson@gmail.com>
2023-10-21 13:00:06 -0400

    Update IMU to test Serial

--- Red Bike Code/IMU_Main/IMU_Main.ino
+++ Red Bike Code/IMU_Main/IMU_Main.ino
@@ -1,13 +1,18 @@
 #include "IMU_Header.h"
-
 void setup() {
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("Hello before init");
   IMU imuInstance;
   imuInstance.IMUClasssetup();
+  Serial.println("Hello after init");
 }
 
 void loop() {
   IMU imuInstance;
+  Serial.println("hello");
   float* result = imuInstance.IMUClassloop();
+  Serial.println("after result");
   Serial.println(result[0]);
   // Do something with result
 }
