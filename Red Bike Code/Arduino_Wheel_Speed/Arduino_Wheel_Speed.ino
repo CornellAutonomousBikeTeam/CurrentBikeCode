affcef1545fa8846fa5a281caad2ce2e60ef783d
Yanjun Zhao <yanjunzhao81@gmail.com>
2023-12-02 13:02:13 -0500

    started creating integration module to Integrate all parts of electrical code. converted Arduino_Wheel_Speed to separate Wheel_Speed_Module Class

--- Red Bike Code/Arduino_Wheel_Speed/Arduino_Wheel_Speed.ino
+++ Red Bike Code/Arduino_Wheel_Speed/Arduino_Wheel_Speed.ino
@@ -1,3 +1,4 @@
+
 const byte FRONTPWM = 0;
 unsigned long FRONT_PWM_TIMER;
 const byte I_2 = 1;
@@ -7,36 +8,37 @@ const byte REARTIMER = 4;
 unsigned long REARTIMER_TIMER;
 const byte REVERSE = 5;
 
-
-
-
-inline void reset1(unsigned long * timerstart) {
+inline void reset1(unsigned long *timerstart)
+{
   *timerstart = micros();
 }
 
-inline void  read1(unsigned long * timerstart) {
+inline void read1(unsigned long *timerstart)
+{
   unsigned long currentTime1 = micros();
-  if(currentTime1 > *timerstart){
+  if (currentTime1 > *timerstart)
+  {
     unsigned long RearTimer1 = currentTime1 - *timerstart;
     Serial.println(RearTimer1);
   }
 }
 
-
-void checkREARTIMER() {
+void checkREARTIMER()
+{
   read1(&REARTIMER_TIMER);
   reset1(&REARTIMER_TIMER);
 }
 
-void checkFRONTPWM() {
+void checkFRONTPWM()
+{
   if (digitalRead(FRONTPWM))
     reset1(&FRONT_PWM_TIMER);
   else
     read1(&FRONT_PWM_TIMER);
-  
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(1000000);
   // Set switch pin as INPUT with pullup
   pinMode(REARTIMER, INPUT);
@@ -45,9 +47,10 @@ void setup() {
 
   // Attach Interrupt to Interrupt Service Routine
   attachInterrupt(digitalPinToInterrupt(REARTIMER), checkREARTIMER, CHANGE); // Pin to REARTIMER
-  attachInterrupt(digitalPinToInterrupt(FRONTPWM), checkFRONTPWM, CHANGE); // Pin to REARTIMER
+  attachInterrupt(digitalPinToInterrupt(FRONTPWM), checkFRONTPWM, CHANGE);   // Pin to REARTIMER
 }
 
-void loop() {
+void loop()
+{
   delay(10);
 }
